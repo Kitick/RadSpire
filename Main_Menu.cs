c3220a40c62c1d1ffd12a9259a92120b81bd13ec
Pablo Macias <pablomacias471@gmail.com>
2025-10-23 19:58:15 -0700

    Main_Menu Script Changes
    Simple starter Code implement for the Main_Menu Script

--- Main_Menu.cs
+++ Main_Menu.cs
@@ -1,23 +1,38 @@
 //Last Editor Info
 //Name: Pablo Macias
-//Date: October 22, 2025
+//Date: October 23, 2025
 //Purpose: Simple Main Menu Screen Layout and Buttons
 
 using Godot;
 using System;
 
 public partial class Main_Menu : Control{
-
-//Start Game Options
-	public void _on_Start_Button_pressed(){
+	
+	public override void _Ready(){
+	//Definitions
+		var startButton = GetNode<Button>("VBoxContainer/Start_Button");
+		startButton.Pressed += _on_Start_Button_pressed;
+		
+		var quitButton = GetNode<Button>("VBoxContainer/Quit_Button");
+		quitButton.Pressed += _on_Quit_Button_pressed;
+		
+		var settingsButton = GetNode<Button>("VBoxContainer/Settings_Button");
+		settingsButton.Pressed += _on_Settings_Button_pressed;
 	}
 
-//Open Settings Menu
-	public void _on_Settings_Button_pressed(){
+	//Implementations
+	//Start Button
+	private void _on_Start_Button_pressed(){
+		GD.Print("Start button was pressed!");
 	}
-
-//Quit Progam
-	public void _on_Quit_Button_pressed(){
+	
+	//Settings Button
+	private void _on_Settings_Button_pressed(){
+		GD.Print("Settings button was pressed");
 	}
-
+	
+	//Quit Button 
+	private void _on_Quit_Button_pressed(){
+		GetTree().Quit();
+	}
 }
