e440d1a54ce1ea46ac3bd1985725f2b423958ae4
justintrinh10 <justintrinh@unr.edu>
2025-10-30 16:28:57 -0700

    Zoom cont

--- Initial Scene/Camera/TopDownCameraPivot.cs
+++ Initial Scene/Camera/TopDownCameraPivot.cs
@@ -6,6 +6,8 @@ public partial class TopDownCameraPivot : Node3D {
     [Export] private Vector3 rotationPresets = new Vector3(-30, -45, -60);
     [Export] private int curTiltIndex;
     [Export] private float zoomSpeed = 2.0f;
+    [Export] private float zoomCoolDown = 1.0f;
+    private Timer zoomTimer = new Timer();
     private Camera3D camera;
     public override void _Ready() {
         base._Ready();
@@ -13,11 +15,39 @@ public partial class TopDownCameraPivot : Node3D {
         Position = calcYZPosVec();
         RotationDegrees = getRotVec();
         camera = GetChild<Camera3D>(0);
+        zoomTimer.OneShot = true;
+        AddChild(zoomTimer);
     }
 
     public override void _Process(double delta) {
         base._Process(delta);
     }
+
+	public override void _Input(InputEvent @event) {
+        base._Input(@event);
+        if(@event is InputEventMouseButton mouseScrollEvent) {
+            if(mouseScrollEvent.Pressed) {
+                if(zoomTimer.IsStopped()) {
+                    zoomTimer.Start(zoomCoolDown);
+                    switch(mouseScrollEvent.ButtonIndex) {
+                        case MouseButton.WheelUp:
+                            if(curTiltIndex < 2) {
+                                curTiltIndex++;
+                            }
+                            break;
+                        case MouseButton.WheelDown:
+                            if(curTiltIndex > 0) {
+                                curTiltIndex--;
+                            }
+                            break;
+                    }
+                    Position = calcYZPosVec();
+                    RotationDegrees = getRotVec();
+                }
+
+            }
+        }
+	}
 
     private Vector3 calcYZPosVec() {
         Vector3 curPosition = new Vector3(0, 0, 0);
