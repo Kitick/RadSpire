3b161dffd47acc4d11a29d82340249eefaa0a141
justintrinh10 <justintrinh@unr.edu>
2025-10-29 22:42:39 -0700

    Implemented top down camera that follows character when exits a center zone

--- Initial Scene/Camera/TopDownCameraPivot.cs
+++ Initial Scene/Camera/TopDownCameraPivot.cs
@@ -1,6 +1,18 @@
 using Godot;
 using System;
 
-public partial class TopDownCameraPivot : Node3D
-{
+public partial class TopDownCameraPivot : Node3D {
+    [Export] private Vector3 default_position = new Vector3(0, 8, 8);
+    [Export] private Vector3 default_rotation = new Vector3(-45, 0, 0);
+    private Camera3D camera;
+    public override void _Ready() {
+        base._Ready();
+        Position = default_position;
+        RotationDegrees = default_rotation;
+        camera = GetChild<Camera3D>(0);
+    }
+
+	public override void _Process(double delta) {
+		base._Process(delta);
+	}
 }
--- Initial Scene/Camera/TopDownCameraRig.cs
+++ Initial Scene/Camera/TopDownCameraRig.cs
@@ -1,6 +1,45 @@
 using Godot;
 using System;
+using System.Runtime;
 
-public partial class TopDownCameraRig : Node3D
-{
+public partial class TopDownCameraRig : Node3D {
+    [Export] public Node3D target;
+    [Export] private Vector2 centerZone = new Vector2(5, 4);
+    [Export] private float followSpeed = 5.0f;
+
+    private Vector3 targetPosition;
+
+    public override void _Ready() {
+        if(target == null) {
+            GD.Print("TD Camera Rig needs a target");
+        }
+        else {
+            GD.Print("TD Camera Rig target is set");
+            targetPosition = target.GlobalPosition;
+            GlobalPosition = targetPosition;
+        }
+    }
+
+    public override void _PhysicsProcess(double delta) {
+        float dt = (float)delta;
+        followTarget(dt);
+    }
+    
+    private void followTarget(float delta) {
+        Vector3 targetPosition = target.GlobalPosition;
+        Vector3 curPosition = GlobalPosition;
+        Vector3 positionDiff = targetPosition - curPosition;
+        bool inCenterZone = true;
+        if(Math.Abs(positionDiff.X) > centerZone.X / 2) {
+            inCenterZone = false;
+        }
+        if(Math.Abs(positionDiff.Z) > centerZone.Y / 2) {
+            inCenterZone = false;
+        }
+        if(!inCenterZone) {
+            float weight = 1f - Mathf.Exp(-followSpeed * delta);
+            GlobalPosition = GlobalPosition.Lerp(target.GlobalPosition, weight);
+        }
+    }
+
 }
