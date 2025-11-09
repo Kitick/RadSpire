6e395c4f5bbd00de5a584994a35b9ff5f5f9303c
Pablo Macias <pablomacias471@gmail.com>
2025-11-09 15:45:33 -0800

    Display_Panel.cs changes
    Updated code for the display panel that still needs to be tested and fixed this is just a little starter

--- Settings Menu/Panel_Scripts/Display_Panel.cs
+++ Settings Menu/Panel_Scripts/Display_Panel.cs
@@ -1,6 +1,95 @@
 using Godot;
 using System;
+using System.Linq;
 
-public partial class Display_Panel : VBoxContainer
-{
+public partial class Display_Panel : VBoxContainer {
+    private OptionButton resolutionOption = null!;
+    private CheckBox fullscreenCheckBox = null!;
+    private CheckBox vsyncCheckBox = null!;
+    private HSlider brightnessSlider = null!;
+    private OptionButton fpsCapOption = null!;
+
+    public override void _Ready() {
+        GetComponents();
+        PopulateResolutionOptions();
+        PopulateFPSCapOptions();
+        SetCallBacks();
+    }
+
+    private void GetComponents() {
+        resolutionOption = GetNode<OptionButton>("Resolution/OptionButton");
+        fullscreenCheckBox = GetNode<CheckBox>("Fullscreen/CheckBox");
+        vsyncCheckBox = GetNode<CheckBox>("VSync/CheckBox");
+        brightnessSlider = GetNode<HSlider>("Brightness/HSlider");
+        fpsCapOption = GetNode<OptionButton>("FPS_Cap/OptionButton");
+    }
+
+    private void SetCallBacks() {
+        resolutionOption.ItemSelected += OnResolutionSelected;
+        fullscreenCheckBox.Toggled += OnFullscreenToggled;
+        vsyncCheckBox.Toggled += OnVsyncToggled;
+        brightnessSlider.ValueChanged += OnBrightnessChanged;
+        fpsCapOption.ItemSelected += OnFPSCapSelected;
+    }
+
+    //Resolution
+    private void PopulateResolutionOptions() {
+        resolutionOption.Clear();
+        resolutionOption.AddItem("1920x1080");
+        resolutionOption.AddItem("1280x720");
+    }
+
+    private void OnResolutionSelected(long index) {
+        string selected = resolutionOption.GetItemText((int)index);
+        string[] parts = selected.Split('x');
+        int width = int.Parse(parts[0]);
+        int height = int.Parse(parts[1]);
+        DisplayServer.WindowSetSize(new Vector2I(width, height));
+    }
+
+    //FullScreen
+    private void OnFullscreenToggled(bool pressed) {
+        DisplayServer.WindowSetMode(pressed ? DisplayServer.WindowMode.Fullscreen : DisplayServer.WindowMode.Windowed);
+        GD.Print($"Full Screen toggled: {pressed}");
+    }
+
+    //VSync
+    private void OnVsyncToggled(bool pressed) {
+        ProjectSettings.SetSetting("display/window/vsync/use_vysnc", pressed);
+        ProjectSettings.Save();
+    }
+
+    //Brightness
+    private void OnBrightnessChanged(double value) {
+        GD.Print($"Brightness set to: {value}");
+    }
+
+    //FPS Cap
+    private void PopulateFPSCapOptions() {
+        fpsCapOption.Clear();
+        fpsCapOption.AddItem("30 FPS");
+        fpsCapOption.AddItem("60 FPS");
+        fpsCapOption.AddItem("120FPS");
+        fpsCapOption.AddItem("Unlimited");
+    }
+
+    private void OnFPSCapSelected(long index) {
+        string selected = fpsCapOption.GetItemText((int)index);
+
+        if(selected == "Unlimited") {
+            Engine.MaxFps = 0;
+            return;
+        }
+
+        //Extract digits
+        string digitsOnly = new string(selected.Where(char.IsDigit).ToArray());
+
+        if(int.TryParse(digitsOnly, out int fps)) {
+            Engine.MaxFps = fps;
+        }
+        else {
+            GD.PushError($"Failed to parse FPS value from: {selected}");
+        }
+        
+    }
 }
