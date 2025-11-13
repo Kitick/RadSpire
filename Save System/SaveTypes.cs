193f10a36f309cff85d43271899f3f041d252a96
Kevin Munson <kitick538@gmail.com>
2025-11-12 20:44:54 -0800

    moved character data to its own file

--- Save System/SaveTypes.cs
+++ Save System/SaveTypes.cs
@@ -34,42 +34,4 @@ namespace SaveSystem {
 		public CameraPivotData CameraPivot { get; init; }
 		public CameraRigData CameraRig { get; init; }
 	}
-
-	public readonly record struct CharacterData : ISaveData {
-		public float CurrentHealth { get; init; }
-		public string CharacterName { get; init; }
-		public float MaxHealth { get; init; }
-		public bool IsInvincible { get; init; }
-		public bool IsAlive { get; init; }
-		public Vector3 Position { get; init; }
-		public Vector3 Rotation { get; init; }
-		public Vector3 Velocity { get; init; }
-		public float Speed { get; init; }
-		public float SpeedModifier { get; init; }
-		public float RotationSpeed { get; init; }
-		public float FallAcceleration { get; init; }
-		public float JumpForce { get; init; }
-		public string Type { get; init; }
-		public bool UseGravity { get; init; }
-		public Vector3 MoveDirection { get; init; }
-		public Vector3 FaceDirection { get; init; }
-		public bool InAir { get; init; }
-		public bool Moving { get; init; }
-	}
-
-	public readonly record struct MonsterData : ISaveData {
-
-	}
-
-	public readonly record struct ZombieData : ISaveData {
-
-	}
-
-	public readonly record struct NPCData : ISaveData {
-
-	}
-
-	public readonly record struct CollectableNPCData : ISaveData {
-
-	}
 }
