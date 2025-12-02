1f0f9e5662037de6f389f28695381bf7b0716996
Kevin Munson <kitick538@gmail.com>
2025-12-01 22:41:10 -0800

    Removed savesystem from host panel

--- UI/HostPanel/HostPanel.cs
+++ UI/HostPanel/HostPanel.cs
@@ -1,68 +1,33 @@
-using Godot;
 using System;
-using System.Collections.Generic;
+using Godot;
 using InputSystem;
-using SaveSystem;
 
 namespace Host {
-    public sealed partial class HostPanel : Control, ISaveable<HostPanelData> {
-        public event Action? OnMenuClosed;
-        private event Action? OnExit;
+	public sealed partial class HostPanel : Control {
+		public event Action? OnMenuClosed;
+		private event Action? OnExit;
 
-        private const string SAVEFILE = "host";
-
-        public override void _Ready() {
-            ProcessMode = ProcessModeEnum.Always;
-
-            SetInputCallbacks();
-        }
-
-        public override void _ExitTree() {
-            OnExit?.Invoke();
-            OnMenuClosed?.Invoke();
-        }
-
-        private void SetInputCallbacks() {
-            OnExit += ActionEvent.MenuBack.WhenPressed(CloseMenu);
-            OnExit += ActionEvent.MenuExit.WhenPressed(CloseMenu);
-        }
-
-        public void OpenMenu() {
-            LoadData();
-        }
-
-        private void CloseMenu() {
-            SaveData();
-            QueueFree();
-        }
-
-        private void SaveData() {
-            SaveService.Save(SAVEFILE, Serialize());
-        }
-
-        private void LoadData() {
-            if(SaveService.Exists(SAVEFILE)) {
-                var data = SaveService. Load<HostPanelData>(SAVEFILE);
-                Deserialize(data);
-            }
-        }
-
-        private ISaveable<T> CastISaveable<T>(ISaveable<T> saveable) where T : ISaveData {
-			return saveable;
+		public override void _Ready() {
+			ProcessMode = ProcessModeEnum.Always;
+			SetInputCallbacks();
 		}
 
-        public HostPanelData Serialize() => new HostPanelData {
-            
-        };
+		public override void _ExitTree() {
+			OnExit?.Invoke();
+			OnMenuClosed?.Invoke();
+		}
 
-        public void Deserialize(in HostPanelData data) {
-            
-        }
-    }
-}
+		private void SetInputCallbacks() {
+			OnExit += ActionEvent.MenuBack.WhenPressed(CloseMenu);
+			OnExit += ActionEvent.MenuExit.WhenPressed(CloseMenu);
+		}
 
-namespace SaveSystem {
-    public readonly record struct HostPanelData : ISaveData {
-        
-    }
+		public void OpenMenu() {
+
+		}
+
+		public void CloseMenu() {
+			QueueFree();
+		}
+	}
 }
