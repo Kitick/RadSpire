be2e2da64e7745dd11fb1e82908506b049a63984
Pablo Macias <pablomacias471@gmail.com>
2025-12-02 21:42:33 -0800

    Update to JoinPanel.cs
    Update to JoinPanel.cs small implementation for the buttons in the panel

--- UI/JoinPanel/JoinPanel.cs
+++ UI/JoinPanel/JoinPanel.cs
@@ -5,12 +5,29 @@ using InputSystem;
 namespace MultiplayerPanels {
 	public partial class JoinPanel : Control {
 		
+		// Paths for Buttons
+
+		private const string NO_PASSWORD_CHECKBOX = "PanelArea/NoPassword/NoPasswordCheckBox";
+		private const string NOT_FULL_CHECKBOX = "PanelArea/NotFull/NotFullCheckBox";
+		private const string CANCEL_BUTTON = "PanelArea/CancelButton";
+		private const string JOIN_BUTTON = "PanelArea/JoinButton";
+
 		public event Action? OnMenuClosed;
 		private event Action? OnExit;
 
+		// Main
 		public override void _Ready() {
 			ProcessMode = ProcessModeEnum.Always;
+
+			SetCallbacks();
 			SetInputCallbacks();
+		}
+
+		private void SetCallbacks() {
+			GetNode<CheckBox>(NO_PASSWORD_CHECKBOX).Toggled += OnNoPasswordCheckboxPressed;
+			GetNode<CheckBox>(NOT_FULL_CHECKBOX).Toggled += OnNotFullCheckboxPressed;
+			GetNode<Button>(CANCEL_BUTTON).Pressed += OnCancelButtonPressed;
+			GetNode<Button>(JOIN_BUTTON).Pressed += OnJoinButtonPressed;
 		}
 
 		public override void _ExitTree() {
@@ -22,6 +39,25 @@ namespace MultiplayerPanels {
 			OnExit += ActionEvent.MenuBack.WhenPressed(CloseMenu);
 			OnExit += ActionEvent.MenuExit.WhenPressed(CloseMenu);
 		}
+		
+		// CallBacks
+		private void OnNoPasswordCheckboxPressed(bool pressed) {
+			//Implementation Here
+			GD.Print($"No Password Toggled: {pressed}");
+		}
+
+		private void OnNotFullCheckboxPressed(bool pressed) {
+			//Implementation Here
+			GD.Print($"No Password Toggled: {pressed}");
+		}
+		private void OnCancelButtonPressed() {
+			CloseMenu();  
+		}
+		
+		private void OnJoinButtonPressed() {
+			//Implementation Here
+			GD.Print("Join Button Pressed!");
+		}
 
 		public void OpenMenu() {
 
@@ -30,5 +66,7 @@ namespace MultiplayerPanels {
 		public void CloseMenu() {
 			QueueFree();
 		}
+
+
 	}
 }
