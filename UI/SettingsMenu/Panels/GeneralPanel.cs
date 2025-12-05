db409853da4443c90f17c8f047c50b642adc817c
Pablo Macias <pablomacias471@gmail.com>
2025-12-04 19:55:00 -0800

    Update to Settings
    Fix to visual panels in display and general settings as well as small code added to Settings panel

--- UI/SettingsMenu/Panels/GeneralPanel.cs
+++ UI/SettingsMenu/Panels/GeneralPanel.cs
@@ -3,6 +3,32 @@ using Godot;
 
 namespace Settings {
 	public sealed partial class GeneralPanel : VBoxContainer {
+		public static readonly bool Debug = false;
 
+		// Node Paths
+
+
+		// Main
+		public override void _Ready() {
+			
+			GetComponents();
+			SetCallbacks();
+		}
+
+		// Get Components
+		private void GetComponents() {
+			
+		}
+
+		// Set Callbacks
+		private void SetCallbacks() {
+			
+		}
+	}
+}
+
+namespace SaveSystem {
+	public readonly record struct GeneralSettings : ISaveData {
+		
 	}
 }
