cbe3273b514c43e79cee0c412f72351a0342a30e
justintrinh10 <justintrinh@unr.edu>
2026-01-25 18:05:41 -0800

    Create new class for ItemDefinition

---
+++ Item/ItemDefinition.cs
@@ -0,0 +1,61 @@
+namespace ItemSystem {
+    using System;
+    using System.Collections.Generic;
+    using Components;
+    using Godot;
+    using Services;
+
+    //static definition of an item
+    [GlobalClass]
+    public partial class ItemDefinition : Resource {
+        [Export]
+        public string Id {
+            get;
+            set {
+                if(string.IsNullOrWhiteSpace(value)) {
+                    return;
+                }
+                field = value;
+            }
+        } = "ItemDefault";
+
+        [Export]
+        public string Name {
+            get;
+            set {
+                if(string.IsNullOrWhiteSpace(value)) {
+                    return;
+                }
+                field = value;
+            }
+        } = "Default Item";
+
+        [Export]
+        public string Description {
+            get;
+            set {
+                if(string.IsNullOrWhiteSpace(value)) {
+                    return;
+                }
+                field = value;
+            }
+        } = "Default Description";
+
+        [Export]
+        public int MaxStackSize {
+            get; set {
+                if(value < 1) {
+                    field = 1;
+                    return;
+                }
+                field = value;
+            }
+        } = 1;
+
+        public bool IsStackable => MaxStackSize > 1;
+
+        [Export] public Texture2D IconTexture { get; set; } = null!;
+
+        [Export] public Godot.Collections.Array<Resource> ComponentsResources { get; set; } = new();
+    }
+}
