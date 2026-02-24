fcf692739a463484003cc33430e125614214d5b9
justintrinh10 <justintrinh@unr.edu>
2026-02-23 21:52:42 -0800

    Created Item3DIconManager

---
+++ Item/ItemIcon3D/Item3DIconManager.cs
@@ -0,0 +1,59 @@
+namespace ItemSystem {
+    using Godot;
+    using Services;
+    using Objects;
+	using System.Collections.Generic;
+
+    public partial class Item3DIconManager : Node, ISaveable<Item3DIconManagerData> {
+        private static readonly LogService Log = new(nameof(Item3DIconManager), enabled: true);
+
+        public List<Item3DIcon> ActiveItem3DIcons { get; private set; } = new List<Item3DIcon>();
+
+        private void AddItem3DIcon(Item3DIcon icon) {
+            ActiveItem3DIcons.Add(icon);
+        }
+
+        private void RemoveItem3DIcon(Item3DIcon icon) {
+            ActiveItem3DIcons.Remove(icon);
+
+        }
+
+        public void SpawnItem(string itemID, Vector3 position, float scaleFactor = 1.0f) {
+            Item? item = ItemDataBaseManager.Instance.CreateItemInstanceById(itemID);
+            if(item == null) {
+                Log.Error($"Failed to load item with ID: {itemID}");
+                return;
+            }
+            Item3DIcon item3DIcon = new Item3DIcon();
+            item3DIcon.Item = item;
+            item3DIcon.Name = item.Name + "3DIcon";
+            AddChild(item3DIcon);
+            item3DIcon.ScaleFactor = scaleFactor;
+            item3DIcon.SpawnItem3D(position);
+            AddItem3DIcon(item3DIcon);
+        }
+        
+        public void DespawnItem(Item3DIcon icon) {
+            RemoveItem3DIcon(icon);
+            icon.QueueFree();
+        }
+
+        public Item3DIconManagerData Export() => new Item3DIconManagerData {
+            Item3DIconsData = ActiveItem3DIcons.ConvertAll(icon => icon.Export())
+		};
+
+		public void Import(Item3DIconManagerData data) {
+			ActiveItem3DIcons.Clear();
+			foreach (Item3DIconData Item3DIconData in data.Item3DIconsData) {
+				var icon = new Item3DIcon();
+				icon.Import(Item3DIconData);
+                ActiveItem3DIcons.Add(icon);
+                
+			}
+		}
+    }
+
+    public readonly record struct Item3DIconManagerData : ISaveData {
+        public List<Item3DIconData> Item3DIconsData { get; init; }
+    }
+}
