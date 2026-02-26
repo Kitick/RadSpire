d2324bd6efc2cdf86425de971d15e10417d251ad
justintrinh10 <justintrinh@unr.edu>
2026-02-25 20:48:24 -0800

    Create World Object Nodes

--- Item/WorldObjects/WorldObjectManager.cs
+++ Item/WorldObjects/WorldObjectManager.cs
@@ -5,7 +5,40 @@ namespace Objects {
     using ItemSystem;
 	using System.Collections.Generic;
 
-	public partial class WorldObjects : ISaveable<WorldObjectsData> {
+    public partial class WorldObjectManager : Node {
+
+    }
+
+    public partial class WorldObjectNodes {
+        private static readonly LogService Log = new(nameof(WorldObjectNodes), enabled: true);
+        private readonly Node ParentNode;
+        private readonly Dictionary<string, ObjectNode> ObjectNodes = new Dictionary<string, ObjectNode>();
+
+        public WorldObjectNodes(Node parent) {
+            ParentNode = parent;
+        }
+
+        public bool AddObjectNode(ObjectNode node) {
+            if(node == null || ObjectNodes.ContainsKey(node.Data.Id)) {
+                return false;
+            }
+            ObjectNodes.Add(node.Data.Id, node);
+            ParentNode.AddChild(node);
+            return true;
+        }
+
+        public bool RemoveObjectNode(string objectId) {
+            if(!ObjectNodes.ContainsKey(objectId)) {
+                return false;
+            }
+            ObjectNode node = ObjectNodes[objectId];
+            ParentNode.RemoveChild(node);
+            ObjectNodes.Remove(objectId);
+            return true;
+        }
+    }
+
+    public partial class WorldObjects : ISaveable<WorldObjectsData> {
         private static readonly LogService Log = new(nameof(WorldObjects), enabled: true);
 
         public Dictionary<string, Object> Objects { get; private set; } = new Dictionary<string, Object>();
@@ -33,7 +66,7 @@ namespace Objects {
         public WorldObjectsData Export() => new WorldObjectsData {
             Objects = ExportObjects()
         };
-        
+
         public Dictionary<string, ObjectData> ExportObjects() {
             Dictionary<string, ObjectData> data = new Dictionary<string, ObjectData>();
             foreach((string id, Object obj) in Objects) {
