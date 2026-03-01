ef1a083b309192ad7fd41489d4af0b276f472ab5
justintrinh10 <justintrinh@unr.edu>
2026-02-28 16:41:46 -0800

    Create componentDictionary to hold item componnents

---
+++ Components/ComponentDictionary.cs
@@ -0,0 +1,48 @@
+namespace Components {
+	using System;
+    using System.Collections.Generic;
+    using Services;
+
+	public class ComponentDictionary<TComponent> where TComponent : class {
+        private static readonly LogService Log = new(nameof(ComponentDictionary<TComponent>), enabled: true);
+		private readonly Dictionary<Type, TComponent> Components = new();
+
+		public int Count => Components.Count;
+
+		public IReadOnlyDictionary<Type, TComponent> All => Components;
+
+		public bool Add<T>(T component) where T : class, TComponent {
+			if(component == null) {
+				return false;
+			}
+			Type key = typeof(T);
+			if(Components.ContainsKey(key)) {
+				return false;
+			}
+			Components[key] = component;
+			return true;
+		}
+
+		public bool Has<T>() where T : class, TComponent {
+			return Components.ContainsKey(typeof(T));
+		}
+
+        public T Get<T>() where T : class, TComponent {
+            if(Has<T>()) {
+                return (T) Components[typeof(T)];
+            }
+            else{
+                Log.Info($"Component of type {typeof(T).Name} not found.");
+                return null!;
+            }
+        }
+
+		public bool Remove<T>() where T : class, TComponent {
+			return Components.Remove(typeof(T));
+		}
+
+		public void Clear() {
+			Components.Clear();
+		}
+	}
+}
