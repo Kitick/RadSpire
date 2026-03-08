df690fdc1296e8a5cf69896a5e470f3cc813dd62
justintrinh10 <justintrinh@unr.edu>
2026-03-08 00:35:38 -0800

    Fixes to objectplacement ui

--- Item/WorldObjects/ObjectPlacementUI.cs
+++ Item/WorldObjects/ObjectPlacementUI.cs
@@ -11,13 +11,38 @@ namespace Objects {
 		private static readonly Color InvalidPreviewColor = new Color(1.0f, 0.2f, 0.2f, 0.65f);
 
 		private Node3D? PreviewNode;
+		private ObjectPlacementManager? ObjectPlacementManager;
+		private bool IsInitialized;
 		public bool HasPreview => PreviewNode != null && GodotObject.IsInstanceValid(PreviewNode);
 
-		public void Initalize(ObjectPlacementManager objectPlacementManager) {
+		public void Initialize(ObjectPlacementManager objectPlacementManager) {
+			if(IsInitialized) {
+				Log.Info("Initialize called more than once; ignoring duplicate initialization.");
+				return;
+			}
+
+			ObjectPlacementManager = objectPlacementManager;
 			objectPlacementManager.OnPlacingObject += HandleOnPlacingObject;
 			objectPlacementManager.OnPlacingObjectValidChanged += HandleOnPlacingObjectValidChanged;
 			objectPlacementManager.StartPlacingObject += HandleStartPlacingObject;
 			objectPlacementManager.EndPlacingObject += HandleEndPlacingObject;
+			IsInitialized = true;
+		}
+
+		public void Initalize(ObjectPlacementManager objectPlacementManager) {
+			Initialize(objectPlacementManager);
+		}
+
+		public override void _ExitTree() {
+			if(IsInitialized && ObjectPlacementManager != null) {
+				ObjectPlacementManager.OnPlacingObject -= HandleOnPlacingObject;
+				ObjectPlacementManager.OnPlacingObjectValidChanged -= HandleOnPlacingObjectValidChanged;
+				ObjectPlacementManager.StartPlacingObject -= HandleStartPlacingObject;
+				ObjectPlacementManager.EndPlacingObject -= HandleEndPlacingObject;
+			}
+			EndPreview();
+			ObjectPlacementManager = null;
+			IsInitialized = false;
 		}
 
 		public void HandleOnPlacingObject(Vector3 position, Vector3 rotation) {
@@ -99,7 +124,13 @@ namespace Objects {
 			var meshes = new List<MeshInstance3D>();
 			CollectMeshes(node, meshes);
 			foreach(MeshInstance3D mesh in meshes) {
-				StandardMaterial3D material = mesh.MaterialOverride as StandardMaterial3D ?? new StandardMaterial3D();
+				StandardMaterial3D material;
+				if(mesh.MaterialOverride is StandardMaterial3D overrideMaterial) {
+					material = (StandardMaterial3D) overrideMaterial.Duplicate();
+				}
+				else {
+					material = new StandardMaterial3D();
+				}
 				material.Transparency = BaseMaterial3D.TransparencyEnum.Alpha;
 				material.AlbedoColor = tint;
 				material.ShadingMode = BaseMaterial3D.ShadingModeEnum.Unshaded;
