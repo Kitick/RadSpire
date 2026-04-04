dffe9da1eb0a3395389bde1107c1b34cc42d3ae0
justintrinh10 <justintrinh@unr.edu>
2026-04-03 17:30:25 -0700

    Added register/unregister game world

--- ItemSystem/WorldObjects/House/GameWorldManager.cs
+++ ItemSystem/WorldObjects/House/GameWorldManager.cs
@@ -11,6 +11,36 @@ public partial class GameWorldManager : Node, ISaveable<GameWorldManagerData> {
     public string CurrentGameWorldId = null!;
     public Dictionary<string, GameWorld> GameWorlds = new();
 
+    public bool SwitchToGameWorld(string gameWorldId) {
+
+    }
+
+    public bool HasGameWorld(string gameWorldId) {
+        return GameWorlds.ContainsKey(gameWorldId);
+    }
+
+    public GameWorld? GetCurrentGameWorld() {
+        if(CurrentGameWorldId == null) {
+            return null;
+        }
+        if(GameWorlds.TryGetValue(CurrentGameWorldId, out GameWorld? gameWorld)) {
+            return gameWorld;
+        }
+        return null;
+    }
+
+    public bool RegisterGameWorld(GameWorld gameWorld) {
+        if(GameWorlds.ContainsKey(gameWorld.Id)) {
+            return false;
+        }
+        GameWorlds.Add(gameWorld.Id, gameWorld);
+        return true;
+    }
+
+    public bool UnregisterGameWorld(string gameWorldId) {
+        return GameWorlds.Remove(gameWorldId);
+    }
+
     public GameWorldManagerData Export() => new GameWorldManagerData {
         CurrentGameWorldId = CurrentGameWorldId,
         GameWorlds = ExportGameWorlds()
