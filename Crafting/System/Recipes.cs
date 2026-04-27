317efe335022196d88da08c8f23cb4fea751a970
justintrinh10 <justintrinh@unr.edu>
2026-04-27 01:56:53 -0700

    Added more crafting recipes for furniture items

--- Crafting/System/Recipes.cs
+++ Crafting/System/Recipes.cs
@@ -156,6 +156,132 @@ public static class Recipes {
 		Outputs: [new(ItemID.ChestPrecious, 1)]
 	);
 
+	public static readonly CraftingRecipe CraftChair = new(
+		RecipeName: "Craft Wooden Chair",
+		Inputs: [new(ItemID.Wood, 2), new(ItemID.Stick, 1)],
+		Outputs: [new(ItemID.Chair, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftTable = new(
+		RecipeName: "Craft Table",
+		Inputs: [new(ItemID.Wood, 4), new(ItemID.Stick, 2)],
+		Outputs: [new(ItemID.Table, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftCounter = new(
+		RecipeName: "Craft Wooden Counter",
+		Inputs: [new(ItemID.Wood, 5), new(ItemID.IronBar, 1)],
+		Outputs: [new(ItemID.Counter, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftCrate = new(
+		RecipeName: "Craft Crate",
+		Inputs: [new(ItemID.Wood, 3), new(ItemID.Stick, 2)],
+		Outputs: [new(ItemID.Crate, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftCrateBig = new(
+		RecipeName: "Craft Large Crate",
+		Inputs: [new(ItemID.Crate, 1), new(ItemID.Wood, 3), new(ItemID.IronBar, 1)],
+		Outputs: [new(ItemID.CrateBig, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftFirePlace = new(
+		RecipeName: "Craft Fireplace",
+		Inputs: [new(ItemID.Stone, 10), new(ItemID.Wood, 2)],
+		Outputs: [new(ItemID.FirePlace, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftOverhang = new(
+		RecipeName: "Craft Overhang",
+		Inputs: [new(ItemID.Wood, 4), new(ItemID.Stick, 3)],
+		Outputs: [new(ItemID.Overhang, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftShelfSmallDown = new(
+		RecipeName: "Craft Small Shelf (Lower)",
+		Inputs: [new(ItemID.Wood, 2), new(ItemID.Stick, 2)],
+		Outputs: [new(ItemID.ShelfSmallDown, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftShelfSmallUp = new(
+		RecipeName: "Craft Small Shelf (Upper)",
+		Inputs: [new(ItemID.Wood, 2), new(ItemID.Stick, 2)],
+		Outputs: [new(ItemID.ShelfSmallUp, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftShelfBigDown = new(
+		RecipeName: "Craft Large Shelf (Lower)",
+		Inputs: [new(ItemID.ShelfSmallDown, 1), new(ItemID.Wood, 3), new(ItemID.IronBar, 1)],
+		Outputs: [new(ItemID.ShelfBigDown, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftShelfBigUp = new(
+		RecipeName: "Craft Large Shelf (Upper)",
+		Inputs: [new(ItemID.ShelfSmallUp, 1), new(ItemID.Wood, 3), new(ItemID.IronBar, 1)],
+		Outputs: [new(ItemID.ShelfBigUp, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftCoffeeTableBrown = new(
+		RecipeName: "Craft Coffee Table (Brown)",
+		Inputs: [new(ItemID.Wood, 3), new(ItemID.Stick, 2)],
+		Outputs: [new(ItemID.TableCoffeeBrown, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftCoffeeTableGray = new(
+		RecipeName: "Craft Coffee Table (Gray)",
+		Inputs: [new(ItemID.TableCoffeeBrown, 1), new(ItemID.Stone, 3)],
+		Outputs: [new(ItemID.TableCoffeeGray, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftCoffeeTableBlack = new(
+		RecipeName: "Craft Coffee Table (Black)",
+		Inputs: [new(ItemID.TableCoffeeBrown, 1), new(ItemID.IronBar, 2)],
+		Outputs: [new(ItemID.TableCoffeeBlack, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftBarrelDrink = new(
+		RecipeName: "Craft Drink Barrel",
+		Inputs: [new(ItemID.Barrel, 1), new(ItemID.Wood, 2), new(ItemID.IronBar, 1)],
+		Outputs: [new(ItemID.BarrelDrink, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftAppleCider = new(
+		RecipeName: "Craft Apple Cider",
+		Inputs: [new(ItemID.BarrelDrink, 1), new(ItemID.AppleRed, 2), new(ItemID.AppleGreen, 2)],
+		Outputs: [new(ItemID.AppleCider, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftCoffee = new(
+		RecipeName: "Craft Coffee Setup",
+		Inputs: [new(ItemID.BarrelDrink, 1), new(ItemID.Wood, 2), new(ItemID.Stone, 1)],
+		Outputs: [new(ItemID.Coffee, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftTent = new(
+		RecipeName: "Craft Tent",
+		Inputs: [new(ItemID.Wood, 8), new(ItemID.Stick, 6)],
+		Outputs: [new(ItemID.Tent, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftHouseSmall = new(
+		RecipeName: "Craft Small House",
+		Inputs: [new(ItemID.Wood, 20), new(ItemID.Stone, 12), new(ItemID.Stick, 8)],
+		Outputs: [new(ItemID.HouseSmall, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftHouseMedium = new(
+		RecipeName: "Craft Medium House",
+		Inputs: [new(ItemID.Wood, 32), new(ItemID.Stone, 20), new(ItemID.IronBar, 6), new(ItemID.Stick, 10)],
+		Outputs: [new(ItemID.HouseMedium, 1)]
+	);
+
+	public static readonly CraftingRecipe CraftHouse1 = new(
+		RecipeName: "Craft House",
+		Inputs: [new(ItemID.Wood, 45), new(ItemID.Stone, 28), new(ItemID.IronBar, 10), new(ItemID.GoldBar, 3), new(ItemID.Stick, 14)],
+		Outputs: [new(ItemID.House1, 1)]
+	);
+
 	public static readonly CraftingRecipe[] AllRecipes = [
 		SmeltIronChunk,
 		SmeltIronBar,
@@ -173,5 +299,29 @@ public static class Recipes {
 		CraftChestCommon,
 		CraftChestRare,
 		CraftChestPrecious,
+		CraftChair,
+		CraftTable,
+		CraftCounter,
+		CraftCrate,
+		CraftCrateBig,
+		CraftFirePlace,
+		CraftOverhang,
+		CraftShelfSmallDown,
+		CraftShelfSmallUp,
+		CraftShelfBigDown,
+		CraftShelfBigUp,
+		CraftCoffeeTableBrown,
+		CraftCoffeeTableGray,
+		CraftCoffeeTableBlack,
+		CraftBarrelDrink,
+		CraftAppleCider,
+		CraftCoffee,
+		CraftTent,
+		CraftHouseSmall,
+		CraftHouseMedium,
+		CraftHouse1,
+		CraftReturnDoor,
+		CraftReturnDoor2,
+		CraftReturnDoorTent,
 	];
 }
