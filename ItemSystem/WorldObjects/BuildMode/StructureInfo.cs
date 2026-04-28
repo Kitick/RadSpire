aa53ab181d70f2dad57a9da8e4268d2b363e69a8
Kevin Munson <kitick538@gmail.com>
2026-04-27 22:24:31 -0700

    formatted structure info with export

--- ItemSystem/WorldObjects/BuildMode/StructureInfo.cs
+++ ItemSystem/WorldObjects/BuildMode/StructureInfo.cs
@@ -1,34 +1,28 @@
 namespace UI.HUD;
 
-using Character;
-using GameWorld;
 using Godot;
 using ItemSystem.WorldObjects;
 using Root;
 
 public sealed partial class StructureInfo : Control {
-	private Label? NameLabel;
-	private Label? NpcLabel;
-	private Label? ValueLabel;
+	[Export] private Label NameLabel = null!;
+	[Export] private Label NpcLabel = null!;
+	[Export] private Label ValueLabel = null!;
+
 	private BuildModeController? BoundBuildMode;
 	private bool IsBound = false;
 
 	public override void _Ready() {
 		base._Ready();
-		NameLabel = GetNodeOrNull<Label>("NameLabel");
-		ValueLabel = GetNodeOrNull<Label>("ValueLabel");
-		NpcLabel = GetNodeOrNull<Label>("NpcLabel");
+		this.ValidateExports();
 	}
 
 	public void Bind(BuildModeController buildModeController) {
-		if(IsBound) {
-			return;
-		}
+		if(IsBound) { return; }
 		BoundBuildMode = buildModeController;
 		//buildModeController.StructureInfoRefreshRequested += OnStructureInfoRefreshRequested;
 		IsBound = true;
 	}
-
 
 	public override void _ExitTree() {
 		if(BoundBuildMode != null) {
@@ -37,12 +31,9 @@ public sealed partial class StructureInfo : Control {
 			IsBound = false;
 		}
 		base._ExitTree();
-	}	
+	}
 
 	private void OnStructureInfoRefreshRequested(string name, string npc, int value) {
-		if(NameLabel == null || NpcLabel == null || ValueLabel == null) {
-			return;
-		}
 		NameLabel.Text = $"Name: {name}";
 		NpcLabel.Text = $"NPC attached: {npc}";
 		ValueLabel.Text = $"Total value: {value}";
